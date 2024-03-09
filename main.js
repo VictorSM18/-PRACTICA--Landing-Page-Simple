12b475b0a2ec2210c6fa333d5d2554cd288b2001
Victor Samuel <victoress18@gmail.com>
2024-03-08 22:46:23 -0400

    Modo oscuro de vista movil

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ navToggle.addEventListener("click", () => {
 });
 
 const toggleDarkBtn = document.querySelector(".theme-toggle");
+const mobileToggleDarkBtn = document.querySelector(".theme-toggle-mobile");
 const titleTag = document.querySelector("h1");
 const firstParagraphTag = document.querySelector(".first-p");
 const alaradoLogo = document.querySelector("header > img");
@@ -22,6 +23,13 @@ const moonImg = document.querySelector(".moon > img");
 const sunElement = document.querySelector(".sun");
 const sunImg = document.querySelector(".sun > img");
 const navElements = document.querySelectorAll(".item");
+
+mobileToggleDarkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    titleTag.classList.toggle("dark-theme");
+    firstParagraphTag.classList.toggle("dark-theme");
+    alaradoLogo.classList.toggle("dark-theme");
+});
 
 toggleDarkBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
